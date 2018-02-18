5a3e1531074d71f0a210c4920e50b0032fcf4fb7
wangjf <wangjf@home.com>
2018-02-18 20:11:33 +0800

    add

--- config/src/main/java/com/wangjf/myweibo/config/UrlCfg.java
+++ config/src/main/java/com/wangjf/myweibo/config/UrlCfg.java
@@ -7,8 +7,8 @@ package com.wangjf.myweibo.config;
 public class UrlCfg {
 
     //private static String mUrlHost = "http://192.168.43.2/myweibo/";
-    //private static String mUrlHost = "http://192.168.1.100/myweibo/";
-    private static String mUrlHost = "http://114.115.165.40/myweibo/";
+    private static String mUrlHost = "http://192.168.1.100/myweibo/";
+    //private static String mUrlHost = "http://114.115.165.40/myweibo/";
     private static String mUrlGetWeibo = mUrlHost + "";
 
     public static String getUrlHost() {
