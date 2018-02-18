cf617764ef02b10f8faadd1fe951e9acabeb2bff
wangjf <wangjf@home.com>
2018-02-19 07:49:06 +0800

    add

--- loginin/src/androidTest/java/com/wangjf/loginin/ExampleInstrumentedTest.java
+++ loginin/src/androidTest/java/com/wangjf/loginin/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package io.weimu.www.library;
+package com.wangjf.loginin;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.weimu.www.library.test", appContext.getPackageName());
+        assertEquals("com.wangjf.loginin.test", appContext.getPackageName());
     }
 }
--- loginin/src/test/java/com/wangjf/loginin/ExampleUnitTest.java
+++ loginin/src/test/java/com/wangjf/loginin/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package io.weimu.www.library;
+package com.wangjf.loginin;
 
 import org.junit.Test;
 
